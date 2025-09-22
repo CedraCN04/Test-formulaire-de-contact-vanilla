a8502729e1baf03a6b017bf654d1b7242be8619d
GuillaumeCeccoli <guiceccoli@gmail.com>
2025-09-22 20:10:02 +0200

    Démarrage du javzscript avec récupération des éléments du DOM

---
+++ main.js
@@ -0,0 +1,7 @@
+// Récupération des éléments du DOM de manière globale
+
+const form = document.getElementById("contact-form");
+const mailInput = document.getElementById("input-mail");
+const passwordInput = document.getElementById("input-password");
+const submitButton = document.getElementById("submit-button");
+
